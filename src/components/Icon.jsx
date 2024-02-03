31ba52f5c031d58241fc4e8f90284ff2758bfd15
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2024-02-03 16:17:17 +0200

    fix(icon): fix bug in displaying user profile image

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,28 +1,35 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+import { BASE_URL } from '../api/axios';
 import DefaultStyle from '../DefaultStyle';
 
 export default function Icon({ text, link, pic, clickHandler }) {
-	return link ? (
-		<Link to={link}>
-			<IconStyle className='material-symbols-outlined'>{text}</IconStyle>
-		</Link>
-	) : (
-		<IconStyle onClick={clickHandler} className='material-symbols-outlined'>
-			{pic ? <img src={pic} alt={'user profile'} /> : text}
-		</IconStyle>
-	);
+  const imageUrl = pic?.[0] !== 'h' ? BASE_URL + '/' + pic : pic;
+
+  return link ? (
+    <Link to={link}>
+      <IconStyle className='material-symbols-outlined'>{text}</IconStyle>
+    </Link>
+  ) : (
+    <IconStyle onClick={clickHandler} className='material-symbols-outlined'>
+      {imageUrl ? <img className='user-profile' src={imageUrl} alt={'user profile'} /> : text}
+    </IconStyle>
+  );
 }
 
 const IconStyle = styled(DefaultStyle)`
-	width: 50px;
-	aspect-ratio: 1;
-	font-size: 200%;
-	font-weight: bold;
-	img {
-		height: 30px;
-		position: absolute;
-		top: 6px;
-		transform: none;
-	}
+  width: 50px;
+  aspect-ratio: 1;
+  font-size: 200%;
+  font-weight: bold;
+  overflow: hidden;
+  .user-profile {
+    /* height: 30px; */
+    height: 100%;
+    width: 100%;
+    /* position: absolute;
+    top: 6px; */
+    transform: none;
+  }
 `;
